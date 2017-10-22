bf8df8de1191b0c757d69ce2f2b6a624a74bf2e7
Fernando Brito Lopes <camult@gmail.com>
2017-10-21 19:26:07 -0500

    update

--- README.md
+++ README.md
@@ -1,16 +1,15 @@
-# `GDmat-package`: Additive and Dominance Relationship Matrices
+# `GRM`: Calculates the realized additive relationship matrix
 
 ## Description
 
 
- Calculates both realized additive and dominance relationship matrices.
+ Calculates realized additive (genomic) relationship matrix.
 
 
 ## Usage
 
 ```r
 GRM(X, smallValue)
-DRM(X)
 ```
 
 
@@ -18,14 +17,8 @@ DRM(X)
 
 Argument      |Description
 ------------- |----------------
-```X```     |     (numeric, $n$ ) matrix of genotypes with a number of rows identical to the number of SNPs (m) and a number of columns identical to the number of genotyped individuals (n). Values in the matrix are 0 and 2 for homozygous and 1 for heterozygous.
-```smallValue```     |     add a small value to the diagonal to ensure the matrix will always be positive definite.
-
-## Value
-
-
- Additive and Dominance Relationship Matrices.
-
+```X```     |     is a (numeric, $n$ ) matrix of genotypes with a number of rows identical to the number of SNPs (m) and a number of columns identical to the number of genotyped individuals (n). Values in the matrix are 0 and 2 for homozygous and 1 for heterozygous.
+```smallValue```     |     is a numeric valeu that can be added to the diagonal of the genomic relationship matrix (GRM).
 
 ## References
 
@@ -38,13 +31,52 @@ Argument      |Description
 ## Examples
 
 ```r 
- ## Not run:
+ ## Not to run ##
  
- ## GRM(Markers, smallValue=0.01)
- 
- ## RRM(Markers)
+ ## GRM(X, smallValue)
  
  ## End(Not run)
+ 
+ ``` 
+
+# `DRM`: Calculates the realized dominance relationship matrix
+
+## Description
+
+
+ Calculates realized dominance relationship matrix.
+
+
+## Usage
+
+```r
+DRM(X)
+```
+
+
+## Arguments
+
+Argument      |Description
+------------- |----------------
+```X```     |     is a (numeric, $n$ ) matrix of genotypes with a number of rows identical to the number of SNPs (m) and a number of columns identical to the number of genotyped individuals (n). Values in the matrix are 0 and 2 for homozygous and 1 for heterozygous.
+
+## References
+
+
+ VanRaden, Paul M. Efficient methods to compute genomic predictions. Journal of dairy science 91.11 (2008): 4414-4423.
+ 
+ Su G, Christensen OF, Ostersen T, Henryon M, Lund MS. 2012. Estimating Additive and Non-Additive Genetic Variances and Predicting Genetic Merits Using Genome-Wide Dense Single Nucleotide Polymorphism Markers. PLoS ONE 7(9): e45293. doi:10.1371/journal.pone.0045293
+
+
+## Examples
+
+```r 
+ ## Not to run ##
+ 
+ ## DRM(X)
+ 
+ ## End(Not run)
+ 
  ``` 
 
 # `NRM`: Calculates the Pedigree-based Additive Relationship Matrix
--- src/rcpparmaGD.cpp
+++ src/rcpparmaGD.cpp
@@ -2,8 +2,28 @@
 
 // [[Rcpp::depends(RcppArmadillo)]]
 
-// Calculates the realized dominance relationship matrix
-//
+
+//' @title Calculates the realized dominance relationship matrix
+//' 
+//' @description Calculates realized dominance relationship matrix.
+//'
+//' @param X is a (numeric, \eqn{n}{m}) matrix of genotypes with a number of rows identical to the number of SNPs (m) and a number of columns identical to the number of genotyped individuals (n). Values in the matrix are 0 and 2 for homozygous and 1 for heterozygous.
+//' 
+//' @examples
+//' ## Not to run ##
+//' 
+//' ## DRM(X)
+//'
+//' ## End(Not run)
+//' 
+//' @references VanRaden, Paul M. Efficient methods to compute genomic predictions. Journal of dairy science 91.11 (2008): 4414-4423.
+//' 
+//' @references Su G, Christensen OF, Ostersen T, Henryon M, Lund MS. 2012. Estimating Additive and Non-Additive Genetic Variances and Predicting Genetic Merits Using Genome-Wide Dense Single Nucleotide Polymorphism Markers. PLoS ONE 7(9): e45293. doi:10.1371/journal.pone.0045293
+//' 
+//' 
+//' @useDynLib GDmat
+//'
+//' @export DRM
 // [[Rcpp::export]]
 arma::mat DRM(arma::mat X){
   arma::vec p = arma::sum(X,1) / (2*X.n_cols);
@@ -17,8 +37,28 @@ arma::mat DRM(arma::mat X){
 }
 
 
-// Calculates the realized additive relationship matrix
-//
+//' @title Calculates the realized additive relationship matrix
+//' 
+//' @description Calculates realized additive (genomic) relationship matrix.
+//'
+//' @param X is a (numeric, \eqn{n}{m}) matrix of genotypes with a number of rows identical to the number of SNPs (m) and a number of columns identical to the number of genotyped individuals (n). Values in the matrix are 0 and 2 for homozygous and 1 for heterozygous.
+//' @param smallValue is a numeric valeu that can be added to the diagonal of the genomic relationship matrix (GRM).
+//' 
+//' @examples
+//' ## Not to run ##
+//' 
+//' ## GRM(X, smallValue)
+//'
+//' ## End(Not run)
+//' 
+//' @references VanRaden, Paul M. Efficient methods to compute genomic predictions. Journal of dairy science 91.11 (2008): 4414-4423.
+//' 
+//' @references Su G, Christensen OF, Ostersen T, Henryon M, Lund MS. 2012. Estimating Additive and Non-Additive Genetic Variances and Predicting Genetic Merits Using Genome-Wide Dense Single Nucleotide Polymorphism Markers. PLoS ONE 7(9): e45293. doi:10.1371/journal.pone.0045293
+//' 
+//' 
+//' @useDynLib GDmat
+//'
+//' @export GRM
 // [[Rcpp::export]]
 arma::mat GRM(arma::mat X, double smallValue){
   arma::vec p = arma::sum(X,1) / (2*X.n_cols);
@@ -31,3 +71,4 @@ arma::mat GRM(arma::mat X, double smallValue){
   return G;
 }
 
+ 
